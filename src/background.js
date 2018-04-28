7c914787f40018dca258253f548fdd9ad3bad837
Ahmed Ashraf <ahmed29329@gmail.com>
2018-04-28 16:37:16 +0200

    add background.js

---
+++ src/background.js
@@ -0,0 +1,2 @@
+import Gitab from './js/gitab';
+let g = new Gitab();
--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,13 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   	mode: 'development',
-  	entry: './src',
+  	entry: {
+  		main: './src/index.js',
+		background: './src/background.js'
+    },
   	output: {
     	path: path.resolve(__dirname, 'dist'),
-    	filename: '[name].bundle.js'
+    	filename: '[name].js'
   	},
   	module: {
     rules: [
